8635e4a21e676fa2ab1116e512a82940da91db36
gene shaffer <genenakamura@gmail.com>
2016-04-30 03:04:58 -0700

    Assets,UI,Animation,Basic Scene Setup
    +Got Assets that we will be using
    +Set up UI for the game, will need to add more
    +Added Timer need to combine with game manager
    +Basic Scene has been setup
    +Animation has been broken down and ready to use

---
+++ Assets/kittyDo.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class kittyDo : MonoBehaviour {
+
+    Animator kittyAnimator;
+
+	// Use this for initialization
+	void Start () {
+       kittyAnimator = this.GetComponent<Animator>();
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+        if (Input.GetKey("up"))
+        {
+            DoWalk();
+        }
+
+        if (Input.GetKeyDown("down"))
+        {
+            DoRun();
+        }
+    }
+
+    public void DoWalk()
+    {
+        kittyAnimator.SetInteger("Status", 1);
+    }
+
+    public void DoRun()
+    {
+        kittyAnimator.SetInteger("Status", 2);
+    }
+
+    public void DoIdle()
+    {
+        kittyAnimator.SetInteger("Status", 0);
+    }
+}
---
+++ Assets/mainMenu.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class mainMenu : MonoBehaviour {
+
+    Button StartButton;
+    Button ExitButton;
+    Text TimerText;
+    Text Title;
+
+    //Timer functions
+    float Timer = 0;
+    bool TimerActive = false;
+
+    void Awake()
+    {
+        TimerText = this.transform.Find("Timer").GetComponent<Text>();
+        TimerText.gameObject.SetActive(false);
+        StartButton = this.transform.Find("Start").GetComponent<Button>();
+        ExitButton = this.transform.Find("Exit").GetComponent<Button>();
+        Title = this.transform.Find("Title").GetComponent<Text>();
+
+    }
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+
+    void FixedUpdate()
+    {
+        float minutes = Mathf.Floor(Timer / 60);
+        float seconds = Timer % 60;
+
+        if (TimerActive == true)
+        {
+            Timer += Time.deltaTime;
+           if (minutes >= 1f)
+            TimerText.text = minutes + ":" + Mathf.RoundToInt(seconds);
+           else
+            TimerText.text = (seconds).ToString("F2");
+        }
+    }
+
+    public void TurnOff()
+    {
+        StartButton.gameObject.SetActive(false);
+        ExitButton.gameObject.SetActive(false);
+        Title.gameObject.SetActive(false);
+    }
+
+    public void TurnOn()
+    {
+        StartButton.gameObject.SetActive(true);
+        ExitButton.gameObject.SetActive(true);
+        Title.gameObject.SetActive(true);
+    }
+
+
+    public void ExitPress()
+    {
+        //   Application.Quit();
+        TurnOff();
+        Debug.Log("Exit Pressed");
+
+    }
+
+    public void HitPlay()
+    {
+        TurnOff();
+        OnTimer();
+
+    }
+
+    //turns on timer
+    public void OnTimer()
+    {
+        TimerText.gameObject.SetActive(true);
+        TimerActive = true;
+    }
+    //turns off timer
+    public void OffTimer()
+    {
+        TimerActive = false; 
+    }
+
+    //reset timer to 0
+    public void ResetTimer()
+    {
+        TimerActive = false;
+        TimerText.gameObject.SetActive(false);
+        Timer = 0f;
+    }
+
+}
