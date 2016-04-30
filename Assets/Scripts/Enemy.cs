72ab159cb72d1938daf41a941d85ccc25f4dd615
zxj001 <jasonz001@outlook.com>
2016-04-30 04:40:18 -0700

    Create spawn animation for cat, added two materials for TapArea

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,21 +1,73 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Collider2D))]
 public class Enemy : TapGameObject {
 
+    // distance above from where the enemy spawns
+    const float distanceToDrop = 10f;
+    // time it takes for enemy to drop to 0
+    const float timeToDrop = 1f;
+
+    Animator animator;
+    Collider2D collider;
+    Rigidbody2D rigidbody;
+
+    const float initialSpeed = 5f;
+    public float speed { get; private set; }
+    // The initial target of our enemy, passed by the enemyspawner
+    // This is only used as the initial velocity at spawn and 
+    // should not be null when spawning is done.
+    public Transform target;
+    // If the enemy is in the game area (ready from spawning)
+    // TODO check for this in TapArea
+    public bool doneSpawning { get; private set; }
 
     public void SetDirection(Vector3 dir)
     {
         // TODO implement
         Debug.Log("new direction: " + dir);
     }
+
+    void Awake () {
+        doneSpawning = false;
+        animator = GetComponent<Animator>();
+        collider = GetComponent<Collider2D>();
+        rigidbody = GetComponent<Rigidbody2D>();
+        rigidbody.gravityScale = 0;
+    }
 	// Use this for initialization
 	void Start () {
 	
 	}
-	
+
+    /// <summary>
+    /// Called as an AnimationEvent by the AnimationClip at
+    /// the end of the Drop animation.
+    /// This sets the Enemy as initialized and starts its movement
+    /// </summary>
+    public void DoneSpawning ()
+    {
+        if (target == null) Debug.LogError("target was not initialzed when enemy spawned!");
+        rigidbody.velocity = (target.position - transform.position).normalized * speed;
+        doneSpawning = true;
+    }
 	// Update is called once per frame
-	void Update () {
-	
+	void FixedUpdate () {
+        // Drop the enemy from the sky!
+
 	}
+    protected void OnEnable()
+    {
+        base.OnEnable();
+        speed = initialSpeed;
+        animator.Play("Drop");
+    }
+    protected void OnDisable()
+    {
+        doneSpawning = false;
+        base.OnDisable();
+    }
 }
---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour {
+
+
+    ActiveTapGOPool activeEnemyPool;
+    TapGOPool enemyPool;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
